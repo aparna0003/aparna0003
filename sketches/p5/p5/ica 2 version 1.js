716d571a2053aa21253eb212d7acc43d520d841d
aparna0003 <aparna0003@.github.com>
2020-02-09 23:44:46 +0530

    Update ica 2 version 1.js

--- sketches/p5/p5/ica 2 version 1.js
+++ sketches/p5/p5/ica 2 version 1.js
@@ -7,30 +7,33 @@ let button;
   function preload()
   {
   // pic = loadImage('image(video,0,0)');
-  maskImage = loadImage('finger print 1.png');
- // pic.resize(100,0);
- // maskImage.resize(100,0);
+  fingerprint = loadImage('fingerprint 3.png');
+  // photo.resize(200,200);
+   fingerprint.resize(200,200);
 }
 
 function takesnap()
 {  
-   photo=image(video,0,0);
-  // ellipse(5,10,10,10)
+   photo=image(video,0,0); 
+  // photo.resize(200,200);
+ // ellipse(5,10,10,10)
 }
 
 
 function setup() 
 {  
-  createCanvas(200, 200);
+  createCanvas(500, 500);
   background(51);
   video= createCapture(VIDEO);
-  video.size(200,200);
+  video.size(500,500);
   fill(120,70,30);
   button = createButton('SNAP');
   button.mousePressed(takesnap);
 
-  photo.mask(maskImage);
-  image(maskImage, 0, 0);
+ // photo.mask(fingerprint);
+ // fingerprint.mask(photo);
+  image(fingerprint , 0, 0);
+ // photo.mask(maskImage);
 }
 
   
